72429483bd5d868cc8bc79ea5b96100f0c9e7f9f
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2024-02-14 13:58:22 +0900

    fromのテスト

--- src/components/Form.test.tsx
+++ src/components/Form.test.tsx
@@ -1,5 +1,9 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Form from "./Form";
+
+// userインスタンスを作成
+const user = userEvent.setup();
 
 describe("Form", () => {
   it("初期状態ではテキストは空", () => {
@@ -10,5 +14,14 @@ describe("Form", () => {
     expect(input).toBeInTheDocument();
     // 初期状態は空
     expect(input).toHaveTextContent("");
+  });
+
+  it("入力したテキストがサブミットされる", async () => {
+    render(<Form />);
+    const input = screen.getByPlaceholderText("Enter text");
+    await user.type(input, "test Text");
+    // 上記までで、入力フォームにtest Textと入力した状態になる
+    // test Textが入力された要素があるのかを検証
+    expect(screen.getAllByDisplayValue("test Text")).toBeInTheDocument();
   });
 });
