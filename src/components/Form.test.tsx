0f714743b44653e157dc43b8166f761449aff890
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2024-02-14 14:39:11 +0900

    サブミットボタンのクリックをシュミレート

--- src/components/Form.test.tsx
+++ src/components/Form.test.tsx
@@ -17,11 +17,20 @@ describe("Form", () => {
   });
 
   it("入力したテキストがサブミットされる", async () => {
+    // 何も返さない
+    const alertSpy = jest.spyOn(window, "alert").mockReturnValue();
     render(<Form />);
     const input = screen.getByPlaceholderText("Enter text");
     await user.type(input, "test Text");
     // 上記までで、入力フォームにtest Textと入力した状態になる
     // test Textが入力された要素があるのかを検証
-    expect(screen.getAllByDisplayValue("test Text")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("test Text")).toBeInTheDocument();
+    const button = screen.getByRole("button");
+    // クリックしたい要素を引数にする
+    await user.click(button);
+    // サブミットテッドに入力した文字列が追加された状態で、ウィンドウアラートが呼び出されるようにする
+    expect(alertSpy).toHaveBeenCalledWith("submitted: test Text");
+    // スパイをclear
+    alertSpy.mockRestore();
   });
 });
